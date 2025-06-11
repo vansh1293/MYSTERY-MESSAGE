69d8ae6808eff6997483b7027d43cf7f2d68caff
vansh1293 <aroravansh.com@gmail.com>
2025-06-11 15:04:21 +0530

    minor change

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -4,10 +4,14 @@ import dbConnect from "@/lib/dbConect";
 import UserModel from "@/model/User";
 import { User } from "next-auth";
 import mongoose from "mongoose";
+import { NextResponse } from "next/server";
 
-export async function DELETE(request: Request, context: { params: { messageid: string } }) {
-    const { messageid } = context.params;
-    console.log("Deleting message with ID:", messageid);
+export async function DELETE(req: Request) {
+    const url = new URL(req.url);
+    const messageid = url.pathname.split("/").pop();
+    if (!messageid) {
+        return NextResponse.json({ success: false, message: "Message ID missing" }, { status: 400 });
+    }
     await dbConnect();
     const session = await getServerSession(authOptions);
     const user = session?.user as User;
